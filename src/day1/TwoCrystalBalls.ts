3cb482acb3960306e0db865191eb196e4531dc68
nolann7 <nolann709@gmail.com>
2023-05-02 19:22:46 +0300

    TwoCrystal implementation

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let start = 0;
+    let end = breaks.length;
+    let result = Infinity;
 
+    while (start < end) {
+        let middle = start + Math.floor((end - start) / 2);
+        if (breaks[middle] === false) {
+            start = middle + 1;
+        }
+        if (breaks[middle] === true) {
+            result = Math.min(result, middle);
+            end = middle;
+        }
+    }
+    return result === Infinity ? -1 : result;
 }
