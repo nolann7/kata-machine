ccce850f4c97ddff51268492e49bd3bfec34d748
nolann7 <nolann709@gmail.com>
2023-05-03 13:50:05 +0300

    implemented Stack with singly linked list

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,20 +1,46 @@
-
+type Node<T> = {value:T, next: Node<T>}
 export default class SinglyLinkedList<T> {
     public length: number;
-
+    private head?: Node<T>;
+    private tail?: Node<T>;
     
 
     constructor() {
+      this.head = this.tail = undefined;
+      this.length = 0;
     }
 
     prepend(item: T): void {
+      const node = {value:item} as Node<T>;
+      this.length++
 
+      if (this.length === 0) {
+        this.head = node;
+        this.tail = node;
+        return;
+      }
+
+      const prevHead = this.head!;
+      this.head = node;
+      this.head.next = prevHead;
+      
 }
     insertAt(item: T, idx: number): void {
 
 }
     append(item: T): void {
+      const node = {value:item} as Node<T>;
+      this.length++
 
+      if (this.length === 0) {
+        this.head = node;
+        this.tail = node;
+        return;
+      }
+
+      const prevTail = this.tail!;
+      prevTail.next = node;
+      this.tail = node;
 }
     remove(item: T): T | undefined {
 
@@ -23,6 +49,6 @@ export default class SinglyLinkedList<T> {
 
 }
     removeAt(idx: number): T | undefined {
-
+      
 }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
+type Node<T> = { value: T; next?: Node<T> };
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        const currentHead = this.head;
+        this.head = node;
+        this.head.next = currentHead;
+    }
     pop(): T | undefined {
+      if (!this.head) {
+        return undefined
+      }
 
-}
+      this.length--;
+      
+      const currentHead = this.head;
+      this.head = this.head.next
+      
+      // free
+      currentHead.next = undefined;
+      if (this.length === 0) {
+        this.tail = undefined;
+      }
+
+
+      return currentHead.value;
+
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
