1c197831499c31498d0f3ac0aa7c70d7e2c3b262
nolann7 <nolann709@gmail.com>
2023-05-06 14:09:48 +0300

    implemented QuickSort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,32 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number) {
+    if (lo >= hi) return;
 
+    const pivotIndex = weekSort(arr, lo, hi);
+
+    qs(arr, lo, pivotIndex - 1);
+    qs(arr, pivotIndex + 1, hi);
+}
+
+function swap(arr: number[], index1: number, index2: number) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+function weekSort(arr: number[], lo: number, hi: number): number {
+    let pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi ; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            swap(arr, idx, i);
+        }
+    }
+    idx++;
+    swap(arr, idx, hi);
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
