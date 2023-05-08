e07a660c7014ea55141990794df4a8eebe36aff6
nolann7 <nolann709@gmail.com>
2023-05-08 15:42:38 +0300

    Breath First Search implementation

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,13 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const que: BinaryNode<number>[] = [head];
 
+    while (que.length) {
+        const currentNode = que.shift() as BinaryNode<number>;
+
+        if (currentNode.left) que.push(currentNode.left);
+        if (currentNode.right) que.push(currentNode.right);
+
+        if (needle === currentNode.value) return true;
+    }
+    return false;
 }
