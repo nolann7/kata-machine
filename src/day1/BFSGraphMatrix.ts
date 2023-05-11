eab63d6bf32f9a56604ff2b164c7f50b5d8682e8
nolann7 <nolann709@gmail.com>
2023-05-11 10:41:19 +0300

    Breath First Search on graph represented in Adjacency Matrix;

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,36 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+    seen[source] = true;
+    const q = [source];
 
+    while (q.length) {
+        let current = q.shift() as number;
+        if (current === needle) break;
+
+        const adjacency = graph[current];
+
+        for (let i = 0; i < adjacency.length; i++) {
+            if (adjacency[i] === 0) continue;
+            if (seen[i]) continue;
+
+            q.push(i);
+            seen[i] = true;
+            prev[i] = current;
+        }
+    }
+
+    if (prev[needle] === -1) return null;
+
+    const out: number[] = [];
+    let curr = needle;
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+    return [source, ...out.reverse()];
 }
